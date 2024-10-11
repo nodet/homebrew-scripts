98b714c0bbad8477070791ea461d5f12e3746ece
Xavier Nodet <xavier.nodet@gurobi.com>
2024-10-11 19:25:50 +0200

    Update git-check to 1.0.2

--- Formula/git-check.rb
+++ Formula/git-check.rb
@@ -1,8 +1,8 @@
 class GitCheck < Formula
   desc "A git command to check the current status of a branch with respect to origin"
   homepage "https://github.com/nodet/git-check"
-  url "https://raw.githubusercontent.com/nodet/git-check/1.0.1/git-check"
-  sha256 "4fffc170ac49593a853128741172b556ba040a90808b61795a934d23b82d0e42"
+  url "https://raw.githubusercontent.com/nodet/git-check/1.0.2/git-check"
+  sha256 "ed5b463db02e32fe627e520db3f2f3655035a91d47b9f39fba79e4173eb3fc48"
   license "Unlicense"
 
   def install
